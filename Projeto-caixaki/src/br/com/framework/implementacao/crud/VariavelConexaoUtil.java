6ed1aa48c036023f23ad48233a5d3e4bdf527217
willi <willi@DESKTOP-2J3TRAE>
2023-11-06 01:28:20 -0200

    06/11/2023

---
+++ Projeto-caixaki/src/br/com/framework/implementacao/crud/VariavelConexaoUtil.java
@@ -0,0 +1,12 @@
+package br.com.framework.implementacao.crud;
+
+/**
+ * Nome do caminho JNDI datasource Tomcat
+ * @author willi
+ *
+ */
+public class VariavelConexaoUtil {
+
+	// Configuração somente para banco postgres
+	public static String JAVA_COMP_ENV_JDBC_DATA_SOURCE = "java:/comp/env/jdbc/datasource";
+}
